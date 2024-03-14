4604ccc481c629384ed0f43dbeab36b0924a9ea0
Alfredo Alonso Anchante Moreno <alonso.amoreno@live.com>
2024-03-14 11:55:20 -0500

    Cambios realizados en el código

---
+++ src/app-hppt2.ts
@@ -0,0 +1,48 @@
+import fs from "fs";
+import http2 from "http2";
+
+const server = http2.createSecureServer(
+  {
+    key: fs.readFileSync("./keys/server.key"),
+    cert: fs.readFileSync("./keys/server.crt"),
+  },
+  (req, res) => {
+    console.log(req.url);
+    // res.writeHead(200, { "Content-Type": "text/html" });
+    // res.write(`URL: ${req.url}`);
+    // res.end();
+
+    // const data = {
+    //   name: "John Doe",
+    //   age: 25,
+    // };
+    // res.writeHead(200, { "Content-Type": "application/json" });
+    // res.write(JSON.stringify(data));
+    // res.end();
+
+    if (req.url === "/") {
+      const htmlFile = fs.readFileSync("./public/index.html", "utf-8");
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(htmlFile);
+      return;
+    }
+
+    if (req.url?.endsWith(".js")) {
+      res.writeHead(200, { "Content-Type": "text/javascript" });
+    } else if (req.url?.endsWith(".css")) {
+      res.writeHead(200, { "Content-Type": "text/css" });
+    }
+
+    try {
+      const responseContent = fs.readFileSync(`./public${req.url}`, "utf-8");
+      res.end(responseContent);
+    } catch (error) {
+      res.writeHead(404, { "Content-Type": "text/html" });
+      res.end("<h1>404 Not Found</h1>");
+    }
+  }
+);
+
+server.listen(8080, () => {
+  console.log("Server is listening on port 8080");
+});
