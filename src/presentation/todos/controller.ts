41226730b5d917e0d43f955bd092db9439ff5303
Alfredo Alonso Anchante Moreno <alonso.amoreno@live.com>
2024-03-14 18:01:50 -0500

    Obtener TODO

--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -1,14 +1,29 @@
 import { Request, Response } from "express";
 
+const todos = [
+  { id: 1, text: "Buy milk", createdAt: new Date() },
+  { id: 2, text: "Do homework", createdAt: null },
+  { id: 3, text: "Go to the gym", createdAt: new Date() },
+];
+
 export class TodosController {
   //* Dependency Injection (DI)
   constructor() {}
 
   public getTodos = (req: Request, res: Response) => {
-    res.json([
-      { id: 1, text: "Buy milk", createdAt: new Date() },
-      { id: 2, text: "Do homework", createdAt: null },
-      { id: 3, text: "Go to the gym", createdAt: new Date() },
-    ]);
+    res.json(todos);
+  };
+
+  public getTodoById = (req: Request, res: Response) => {
+    const { id } = req.params;
+    (isNaN(+id) || +id < 1) &&
+      res.status(400).json({ message: "ID argument is not a number!" });
+
+    const todo = todos.find((todo) => todo.id === +id);
+
+    if (!todo) {
+      return res.status(404).json({ message: "Todo not found" });
+    }
+    res.json(todo);
   };
 }
